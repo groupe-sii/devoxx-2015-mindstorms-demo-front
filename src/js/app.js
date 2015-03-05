16ffac0a4d7e09009005e16aec36195063e30727
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-03-05 17:07:39 +0100

    [TECH] Cleaning + add constants module

--- src/js/app.js
+++ src/js/app.js
@@ -11,14 +11,27 @@
  */
 angular.module('devoxx-2015-mindstorms-demo-front', [
 	'ngResource',
-    'templates'
+    'btford.socket-io',
+    'templates',
+    'ev3Config'
 ])
+/**
+ * @ngdoc factory
+ * @name devoxx-2015-mindstorms-demo-front.factory:socketFactory
+ * @description
+ * A description of the socketFactory
+ */
+.factory('appSocketFactory', ['socketFactory', 'ev3IP', function(socketFactory, ev3IP) {
+    return socketFactory({
+        ioSocket: io.connect('http://' + ev3IP.ip + ':3000')
+    });
+}])
 /**
  * @ngdoc controller
  * @name devoxx-2015-mindstorms-demo-front.controller:homeCtrl
  * @description
  * A description of the controller
  */
-.controller('homeCtrl', [function() {
-
+.controller('homeCtrl', ['appSocketFactory', function(appSocketFactory) {
+    console.log(appSocketFactory);
 }]);
