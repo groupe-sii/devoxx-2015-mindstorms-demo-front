077b364b398458ff7f728c046bc6046caec0b572
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-04-03 13:50:50 +0200

    [DEV] controllerAs syntax

--- src/js/controllers/homeController.js
+++ src/js/controllers/homeController.js
@@ -9,7 +9,7 @@
  * A description of the controller
  */
 angular.module('devoxx-2015-mindstorms-demo-front')
-    .controller('homeController', [
+    .controller('HomeController', [
         '$scope',
         '$interval',
         '$timeout',
@@ -18,45 +18,47 @@ angular.module('devoxx-2015-mindstorms-demo-front')
         'DataFactory',
         function($scope, $interval, $timeout, appSocketFactory, MotorService, DataFactory) {
 
-            console.log('homeController');
+            console.log('HomeController');
 
             /**
              * Sensors stuff
              */
 
-            var CORRECT_MESSAGE = 'Correct !',
+            var vm = this,
+                CORRECT_MESSAGE = 'Correct !',
                 INCORRECT_MESSAGE = 'Incorrect dude !',
                 MIN_WIN = 3,
                 TIME_ANSWER = 10,
                 _intervalTimer = null,
                 IMG_FOLDER = 'assets/img/';
 
-            $scope.answered = false;
-            $scope.answerResult = false;
-            $scope.finalResult = false;
-            $scope.timerAnswer = TIME_ANSWER;
-            $scope.backgrdUrl = {
+            vm.answered = false;
+            vm.answerResult = false;
+            vm.finalResult = false;
+            vm.timerAnswer = TIME_ANSWER;
+            vm.question = {};
+            vm.backgrdUrl = {
                 background: 'url(../assets/img/unsplash_5245b69bc5330_1.jpg)'
             };
 
             appSocketFactory.forward('sensorValue', $scope);
 
             var sensorListener = function(ev, data) {
-                $scope.sensorValues = data;
+                vm.sensorValues = data;
 
                 console.log('sensorListener');
 
                 if (data.sensor1 === 1 || data.sensor2 === 1) {
-                    $scope.answered = true;
+                    vm.answered = true;
 
                     appSocketFactory.removeAllListeners();
 
-                    if ((data.sensor1 === 1 && $scope.question.answers[0].correct) || (data.sensor2 === 1 && $scope.question.answers[1].correct)) {
-                        $scope.result = CORRECT_MESSAGE;
-                        $scope.answerResult = true;
-                        $scope.score += 1;
+                    if ((data.sensor1 === 1 && vm.question.answers[0].correct) || (data.sensor2 === 1 && vm.question.answers[1].correct)) {
+                        vm.result = CORRECT_MESSAGE;
+                        vm.answerResult = true;
+                        vm.score += 1;
                     } else {
-                        $scope.result = INCORRECT_MESSAGE;
+                        vm.result = INCORRECT_MESSAGE;
                     }
 
                     $timeout(function() {
@@ -67,8 +69,8 @@ angular.module('devoxx-2015-mindstorms-demo-front')
             };
 
             var updateBackground = function() {
-                $scope.backgrdUrl = {
-                    background: 'url(../' + IMG_FOLDER + $scope.question.bckgrd + ')'
+                vm.backgrdUrl = {
+                    background: 'url(../' + IMG_FOLDER + vm.question.bckgrd + ')'
                 };
             };
 
@@ -82,13 +84,13 @@ angular.module('devoxx-2015-mindstorms-demo-front')
 
             $scope.$on('socket:sensorValue', sensorListener);
 
-            $scope.startSensorListener = function() {
+            vm.startSensorListener = function() {
                 appSocketFactory.emit('startReadingSensors', function() {
                     console.log('startReadingSensors action readed by server');
                 });
             };
 
-            $scope.stopSensorListener = function() {
+            vm.stopSensorListener = function() {
                 appSocketFactory.emit('stopReadingSensors', function() {
                     console.log('stopReadingSensors action readed by server');
                 });
@@ -98,7 +100,7 @@ angular.module('devoxx-2015-mindstorms-demo-front')
              * Motor stuff
              */
 
-            $scope.startMotor = function() {
+            vm.startMotor = function() {
                 MotorService.startMotor().then(function() {
                     console.log('motor started');
                 });
@@ -111,50 +113,50 @@ angular.module('devoxx-2015-mindstorms-demo-front')
             var questions = null,
                 nextQuestion = function() {
                     startTimer();
-                    $scope.questionIndex += 1;
-                    $scope.answered = $scope.answerResult = false;
+                    vm.questionIndex += 1;
+                    vm.answered = vm.answerResult = false;
                     appSocketFactory.forward('sensorValue', $scope);
-                    if ($scope.questionIndex > questions.length) {
+                    if (vm.questionIndex > questions.length) {
                         finishTest();
-                        $scope.finished = true;
+                        vm.finished = true;
                         return;
                     }
-                    $scope.question = questions[$scope.questionIndex - 1];
+                    vm.question = questions[vm.questionIndex - 1];
                     updateBackground();
-                    $scope.result = '';
+                    vm.result = '';
                 },
                 finishTest = function() {
                     appSocketFactory.removeAllListeners();
                     $interval.cancel(_intervalTimer);
-                    if ($scope.score >= MIN_WIN) {
-                        $scope.finalResult = true;
-                        $scope.startMotor();
+                    if (vm.score >= MIN_WIN) {
+                        vm.finalResult = true;
+                        vm.startMotor();
                         playWinningSound(true);
                     } else {
                         playWinningSound();
                     }
                 },
                 startTimer = function() {
-                    $scope.timerAnswer = TIME_ANSWER;
+                    vm.timerAnswer = TIME_ANSWER;
                     _intervalTimer = $interval(function() {
-                        $scope.timerAnswer -= 1;
-                        if ($scope.timerAnswer === 0) {
+                        vm.timerAnswer -= 1;
+                        if (vm.timerAnswer === 0) {
                             $interval.cancel(_intervalTimer);
                             nextQuestion();
                         }
                     }, 1000);
                 };
 
-            $scope.startTest = function() {
-                $scope.testStarted = true;
-                $scope.finished = false;
-                $scope.questionIndex = 1;
-                $scope.score = 0;
+            vm.startTest = function() {
+                vm.testStarted = true;
+                vm.finished = false;
+                vm.questionIndex = 1;
+                vm.score = 0;
 
                 questions = DataFactory.get();
-                $scope.question = questions[$scope.questionIndex - 1];
+                vm.question = questions[vm.questionIndex - 1];
 
-                $scope.startSensorListener();
+                vm.startSensorListener();
                 updateBackground();
                 startTimer();
             };
