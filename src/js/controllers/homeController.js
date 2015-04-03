48f71bc8b784f63e33bba6ef6aa1079e1ba0b034
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-04-03 12:16:36 +0200

    [FIX]

--- src/js/controllers/homeController.js
+++ src/js/controllers/homeController.js
@@ -67,9 +67,8 @@ angular.module('devoxx-2015-mindstorms-demo-front')
             };
 
             var updateBackground = function() {
-                console.log('url(../"' + IMG_FOLDER + $scope.question.bckgrd + '")');
                 $scope.backgrdUrl = {
-                    background: 'url(../"' + IMG_FOLDER + $scope.question.bckgrd + '")'
+                    background: 'url(../' + IMG_FOLDER + $scope.question.bckgrd + ')'
                 };
             };
 
